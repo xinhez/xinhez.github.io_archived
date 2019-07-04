e04940d44ea8fdd6bff2460dbe35eb6781bdafd6
Xinhe Zhang <xinhez@VPN-128-237-147-53.LIBRARY.VPN.CMU.EDU>
2019-07-04 18:52:24 +0800

    Update front cover

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import background from '../img/background.JPG';
 import "./Cover.css";
 
 class Cover extends Component {
   render() {
     return (
       <div className="Cover">
-        Cover
+        <img src={background} className="Cover-img" alt="logo" />
+        <div className="Cover-text">
+          <h1>
+            I'm Xinhe Zhang.
+          </h1>
+          <h2>
+            A CMU ECE Master student graduating in December 2020. Focusing on Machine Learning. Once a full-stack engineer.
+          </h2>
+        </div>
       </div>
     );
   }
